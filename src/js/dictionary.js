3d809a1fe9b10fb343f08ffe41a7253679b94669
Zain <zain.khoso.dev@gmail.com>
2023-06-30 12:35:49 +0500

    Dictionary

--- src/js/dictionary.js
+++ src/js/dictionary.js
@@ -1,74 +1,44 @@
-const search = document.getElementById("search");
-const searchBtn = document.getElementById("searchBtn");
-const word = document.getElementById("word");
-const meaning = document.getElementById("meaning");
-const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    " ",
-];
+"use strict";
 
-function validateInput(input) {
-    if (input.trim() === "") {
-        alert("Don't just play around give me a word to search.");
-        search.value = "";
-    } else if (
-        Array.from(input).every((elem) => alphabet.includes(elem.toLowerCase()))
-    )
-        return true;
-    else {
-        alert("Only alphabet is allowed");
-        search.value = "";
+// DOM Selections
+const search_entry = document.getElementById("search-field");
+const search_btn = document.getElementById("search-btn");
+const word_element = document.getElementById("word-element");
+const defination_element = document.getElementById("defination-element");
+
+// Program Variables
+const alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+
+// Functions
+const fetchMeaning = async function () {
+    const query = search_entry.value;
+
+    if (!queryIsEligible(query)) return;
+
+    try {
+        const request = await fetch(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
+        );
+
+        const data = await request.json();
+
+        word_element.textContent = data[0]["word"];
+        defination_element.textContent =
+            data[0]["meanings"][0]["definitions"][0]["definition"];
+    } catch (err) {
+        word_element.textContent = query;
+        defination_element.textContent = "This is not a valid word.";
+        return;
     }
-}
+};
 
-async function searchWord() {
-    let srch = search.value;
-    let req;
-    let data;
+const queryIsEligible = function (query) {
+    for (let i = 0; i < query.length; i++) {
+        const character = query[i].toLowerCase();
+        if (!alphabet.includes(character)) return false;
+    }
+    return true;
+};
 
-    if (validateInput(srch)) {
-        try {
-            req = await fetch(
-                `https://api.dictionaryapi.dev/api/v2/entries/en/${srch}`
-            );
-            data = await req.json();
-
-            word.textContent = srch;
-            meaning.textContent =
-                data[0]["meanings"][0]["definitions"][0]["definition"];
-            search.value = "";
-        } catch (err) {
-            alert(`Error occured, Please check the spelling.`);
-            search.value = "";
-            return;
-        }
-    } else return;
-}
-
-
-searchBtn.addEventListener("click", searchWord);
+// Event Listeners
+search_btn.addEventListener("click", fetchMeaning);
