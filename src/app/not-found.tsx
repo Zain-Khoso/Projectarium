9fe90f4920072b59c1293d528d0a2a86e0c57461
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-12 22:26:35 +0500

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+// Component.
+export default function NotFound() {
+  return (
+    <main className="w-full min-h-[80vh] grid place-items-center">
+      <h1 className="text-4xl">404 | Page not found.</h1>
+    </main>
+  );
+}
