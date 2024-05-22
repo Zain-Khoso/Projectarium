6e2b787043c9945a3ba0575a142ad4bcc4cb78b9
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-05-22 14:25:22 +0500

    RESET

--- src/components/ColoredText.tsx
+++
@@ -1,9 +0,0 @@
-// Types.
-type Props = {
-  children: React.ReactNode;
-};
-
-// Copmponent.
-export default function ColoredText({ children }: Props) {
-  return <span className="text-green-500">{children}</span>;
-}
--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-// Types.
-type Props = {
-  children: React.ReactNode;
-};
-
-// Component.
-export default function Container({ children }: Props) {
-  return <div className="max-w-[1440px] mx-auto">{children}</div>;
-}
