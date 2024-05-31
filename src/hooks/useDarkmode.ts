72234ceb23bbdb00b22d6246daadc2648fb5d8cd
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-05-31 16:53:15 +0500

    fix: localstorage

--- src/hooks/useDarkmode.ts
+++ src/hooks/useDarkmode.ts
@@ -3,13 +3,15 @@ import { useState, useLayoutEffect } from 'react';
 
 // Hook.
 export default function useDarkmode() {
-  const [darkmode, setDarkmode] = useState(JSON.parse(localStorage.getItem('darkmode') || 'false'));
+  const [darkmode, setDarkmode] = useState(
+    JSON.parse(window.localStorage.getItem('darkmode') || 'false')
+  );
 
   useLayoutEffect(() => {
     if (darkmode) document.body.classList.add('dark');
     else document.body.classList.remove('dark');
 
-    localStorage.setItem('darkmode', darkmode);
+    window.localStorage.setItem('darkmode', darkmode);
   }, [darkmode]);
 
   return [darkmode, setDarkmode];
