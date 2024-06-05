84347bcd17a0ab71d06f79a06a88f9d56e024020
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-06-05 16:32:57 +0500

    fix: infinite scroll rollback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
-// Lib Imports.
-import { collection, query, where, orderBy } from 'firebase/firestore';
-
 // Local Imports.
-import { firestore } from '@/configs/firebase';
-import { fetchDocs } from '@/utils/firebase/firestore';
 import { cn } from '@/utils/utils';
 import { Navbar } from '@/components/Navigation';
 import InfiniteScroll from '@/components/InfiniteScroll';
 
 // Component.
-export default async function Home() {
-  const dataQ = query(
-    collection(firestore, 'projects'),
-    where('lifecycleStatus', '==', 'Published'),
-    orderBy('createdAt', 'desc')
-  );
-  const data = await fetchDocs(dataQ);
-
-  const projects = data?.map((doc) => {
-    return { id: doc.id, ...doc.data() };
-  });
-
+export default function Home() {
   return (
     <main className={cn('min-w-dvw min-h-dvh')}>
       <Navbar />
 
-      <InfiniteScroll projects={projects!} />
+      <InfiniteScroll />
     </main>
   );
 }
