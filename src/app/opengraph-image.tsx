b462a04e6ecebdbf53ddbe0fd01769a95e36975d
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-06-06 14:35:54 +0500

    fix: Metadata Images

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -5,7 +5,7 @@ import { ImageResponse } from 'next/og';
 export const runtime = 'edge';
 
 // Image metadata
-export const alt = 'Share your project';
+export const alt = 'Projectarium';
 export const size = {
   width: 1200,
   height: 630,
