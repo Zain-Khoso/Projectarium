33268420e455c865ba70e5c56a8ac8f267cbc596
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-06-06 13:44:32 +0500

    fix Meta IMAGES

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -3,22 +3,22 @@ import { ImageResponse } from 'next/og';
 
 // Local Imports.
 import { cn } from '@/utils/utils';
+import { H1 } from '@/components/ui/typography';
 
 export const runtime = 'edge';
 export const alt = 'Projectarium';
 export const size = {
   width: 1200,
   height: 630,
-  };
+};
 export const contentType = 'image/png';
-
 
 // Image generation
 export default async function Image() {
   return new ImageResponse(
     (
-      <div className={cn('w-full h-full bg-white flex items-center justify-center text-2xl')}>
-        Projectarium
+      <div className={cn('w-full h-full bg-white flex items-center justify-center')}>
+        <H1 className={cn('text-black')}>Projectarium</H1>
       </div>
     )
   );
