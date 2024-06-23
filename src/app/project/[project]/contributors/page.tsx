26ae6acfec0cecbfca7432c91c3dae0c147bf140
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-06-23 12:41:54 +0500

    new: Project Contributors Metadata

--- src/app/project/[project]/contributors/page.tsx
+++ src/app/project/[project]/contributors/page.tsx
@@ -1,13 +1,15 @@
+// Lib Imports.
+import { Metadata } from 'next';
+
 // Local Imports.
 import { cn } from '@/utils/utils';
-import { H1, Lead } from '@/components/ui/typography';
+
+// Metadata.
+export const metadata: Metadata = {
+  title: 'Contributors of',
+};
 
 // Component.
 export default function ProjectContributors() {
-  return (
-    <main className={cn('min-w-dvw min-h-dvh flex flex-col justify-center items-center gap-2')}>
-      <H1>Project Contributors</H1>
-      <Lead>This page is currently under development.</Lead>
-    </main>
-  );
+  return <main className={cn('')}></main>;
 }
