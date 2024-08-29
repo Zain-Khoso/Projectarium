b83adf3752b8e0a0a2913f6af01c82c6c6395950
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-08-29 21:44:38 +0500

    update: Error UI for the user profile page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,10 +1,14 @@
 // Lib Imports.
 import { Metadata } from 'next';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 // Actions.
 import getCurrentUser from '@/actions/getCurrentUser';
 import getUserByUsername from '@/actions/getUserByUsername';
+
+// Icons.
+import { TbError404 } from 'react-icons/tb';
 
 // Components.
 import Navbar from '@/components/navbar/Navbar';
@@ -57,13 +61,32 @@ export default async function ProfilePage({ params: { username } }: Props) {
       <Navbar currentUser={currentUser} profileUser={profileUser} />
 
       <Container>
-        <main className="min-h-screen flex flex-col md:flex-row gap-8 pt-40 lg:pt-44 pb-8">
-          <UserContent currentUser={currentUser} profileUser={profileUser} />
+        {profileUser ? (
+          <main className="min-h-screen flex flex-col md:flex-row gap-8 pt-40 lg:pt-44 pb-8">
+            <UserContent currentUser={currentUser} profileUser={profileUser} />
 
-          <Suspense>
-            <DynamicTabs currentUser={currentUser} profileUser={profileUser} />
-          </Suspense>
-        </main>
+            <Suspense>
+              <DynamicTabs currentUser={currentUser} profileUser={profileUser} />
+            </Suspense>
+          </main>
+        ) : (
+          <main className="min-h-screen pt-40 lg:pt-44 pb-8 flex flex-col items-center gap-4">
+            <TbError404 size={120} className="fill-rose-500" />
+            <h1 className="text-4xl text-neutral-600">User does not exist.</h1>
+            <p className="text-neutral-600 font-light">
+              Go back to{' '}
+              {currentUser ? (
+                <Link href={`/${currentUser.username}`} className="text-sky-500 font-semibold">
+                  Your profile
+                </Link>
+              ) : (
+                <Link href={'/'} className="text-sky-500 font-semibold">
+                  Home page
+                </Link>
+              )}
+            </p>
+          </main>
+        )}
       </Container>
     </>
   );
