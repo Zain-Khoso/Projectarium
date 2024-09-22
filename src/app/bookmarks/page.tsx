cb8433c2b0f330c3b7576aa7d65414e4fff4a1c1
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-09-22 11:34:19 +0500

    update: metadata and no bookmarks error handling for bookmarks page

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,3 +1,6 @@
+// Lib Imports.
+import { notFound } from 'next/navigation';
+
 // Actions.
 import authenticationRequired from '@/actions/authenticationRequired';
 import getCurrentUser from '@/actions/getCurrentUser';
@@ -13,13 +16,30 @@ import ProjectCard from '@/components/ProjectCard';
 import { Metadata } from 'next';
 
 // Metadata.
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  title: 'Your Bookmarks',
+  description:
+    'View and manage your saved projects on Projectarium. Easily revisit your favorite projects and stay updated with the latest changes.',
+  keywords: [
+    'Projectarium',
+    'bookmarks',
+    'saved projects',
+    'favorite projects',
+    'developer resources',
+    'programmer bookmarks',
+    'project tracking',
+    'code collaboration',
+    'developer favorites',
+  ],
+};
 
 export default async function BookmarksPage() {
   await authenticationRequired();
 
   const currentUser = await getCurrentUser();
   const bookmarks = await getUserBookmarks(currentUser?.bookmarkIds);
+
+  if (bookmarks.length === 0) return notFound();
 
   return (
     <>
