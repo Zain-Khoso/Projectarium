9b26207616467a7433df1b0e2cb7f3d73288cff8
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-09-25 23:39:47 +0500

    fix: Homepage project cards placement

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -15,7 +15,7 @@ export default async function InfiniteScroll({ currentUser }: Props) {
   const projects = await getAllProjects();
 
   return (
-    <main className="grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-8 pt-28 pb-8">
+    <main className="grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-8 place-items-center pt-28 pb-8">
       {projects?.map((project) => (
         <ProjectCard
           key={project.id}
